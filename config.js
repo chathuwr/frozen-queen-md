aad731d10d3a50dbae9b0ae849421df910353228
chathuwr <gitom1514@gmail.com>
2025-03-17 02:35:54 -0400

    Update config.js

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ function convertToBool(text, fault = "true") {
 const now = new Date();
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "MYRwkJoI#lkQ4RPKEG6LkP20VjHOmiafIEms_D_ryhJkIZdh9jN0",
-  OWNER_NUM: process.env.OWNER_NUM || "94702560019",
+  SESSION_ID: process.env.SESSION_ID || "tQ5WlbKR#fqJuGFSO5ckE5-lXsd68qG1W6r_CeKvZF5tYFoXDI-I",
+  OWNER_NUM: process.env.OWNER_NUM || "94713670127",
   PREFIX: process.env.PREFIX || ".",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/chathurahansaka1/help/main/src/cdbe3771-c89d-4ee9-a7d6-254d0c321c8a.jpg",
   ALIVE_MSG: process.env.ALIVE_MSG || `
