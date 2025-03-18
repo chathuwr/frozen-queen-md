9530b871bd17e48db5b31679099d34157f07bbae
chathuwr <gitom1514@gmail.com>
2025-03-18 03:40:35 -0400

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -11,7 +11,7 @@ cmd({
 async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{  
      await robin.sendPresenceUpdate('recording', from);
-     await robin.sendMessage(from, { audio: { url: "https://github.com/chathurahansaka1/help/raw/refs/heads/main/audio/WhatsApp%20Audio%202025-03-12%20at%2010.43.34%20AM.mp4" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+     await robin.sendMessage(from, { audio: { url: "https://github.com/chathurahansaka1/help/raw/refs/heads/main/audio/WhatsApp%20Audio%202025-03-18%20at%203.25.32%20AM.mpeg" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
       await robin.sendMessage(from,{sticker: { url :"https://github.com/chathurahansaka1/help/raw/refs/heads/main/sticker/cebf62bc-2fe8-444d-9416-282ccaf826c2.webp"},package: '❄️Frozen Queen❄️'},{ quoted: mek }) 
 return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
     
